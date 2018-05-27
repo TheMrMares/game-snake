57b3d735feb04899393ecde69f5b7c235a70ef72
Unknown <rak.bartosz98@gmail.com>
2018-05-27 02:30:50 +0100

    Apple lifecycle and growing

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded',function(){
     myCanvas = document.getElementById('myCanvas');
     ctx = myCanvas.getContext('2d');
-    window.setInterval(game, 1000/20);
+    window.setInterval(game, 1000/10);
     window.addEventListener('keydown', pushKey);
 });
 
@@ -10,22 +10,58 @@ t = 20;
 px = py = 10*t;
 vx = vy = 0;
 ax = ay = 0;
+size = 5;
+tail = []
 
+createApple();
 function game(){
-    console.log("one");
     px += vx*t;
     py += vy*t;
+
+    if( px < 0){
+        px = 24*t;
+    }
+    if( px > cwidth){
+        px = 0*t;
+    }
+    if( py > cheight) {
+        py = 0*t;
+    }
+    if( py < 0) {
+        py = 24*t;
+    }
+
+    if(px == ax && py == ay){
+        console.log("jablko"+size);
+        size++;
+        createApple();
+    }
 
     ctx.fillStyle = 'black';
     ctx.fillRect(0,0,cwidth,cheight);
 
     ctx.fillStyle = 'green';
-    ctx.fillRect(px,py,20,20);
+    ctx.fillRect(px+1,py+1,20-2,20-2);
+    
+    for(var i = 0;i < tail.length;i++){
+        ctx.fillStyle = 'green';
+        ctx.fillRect(tail[i].x+1,tail[i].y+1,20-2,20-2);
+        if(tail[i].x == px && tail[i].y == py){
+            size = 5;
+        }
+    }
+    tail.push({x: px, y: py});
+    if(tail.length > size){
+        tail.shift();
+    }
+
+    ctx.fillStyle = 'orange';
+    ctx.fillRect(ax+1,ay+1,20-2,20-2);
 }
 
 function createApple(){
-    ax = t*(Math.random(25));
-    ay = t*(Math.random(25));
+    ax = t*(Math.floor((Math.random() * 25) + 0));
+    ay = t*(Math.floor((Math.random() * 25) + 0));
 }
 
 function pushKey(evt){
